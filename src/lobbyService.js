27f986294e60052693735dbbbc00c68ab067c283
massatm <dennilotz@googlemail.com>
2026-08-06 21:00:24 +0200

    Add multiplayer lobby player management

--- src/lobbyService.js
+++ src/lobbyService.js
@@ -1,11 +1,29 @@
 import { ref, set, update, onValue, get } from 'firebase/database';
 import { database } from './firebase';
 
-export function createLobby(code, data) {
-  return set(ref(database, `lobbies/${code}`), data);
+export const MAX_PLAYERS = 4;
+
+export function createLobby(code, player) {
+  const lobby = {
+    code,
+    players: {
+      [player.id]: player,
+    },
+    status: 'waiting',
+    draft: null,
+  };
+
+  return set(ref(database, `lobbies/${code}`), lobby);
 }
 
-export function joinLobby(code, player) {
+export async function joinLobby(code, player) {
+  const lobby = await getLobby(code);
+  const players = lobby?.players || {};
+
+  if (Object.keys(players).length >= MAX_PLAYERS) {
+    throw new Error('Lobby is full');
+  }
+
   return update(ref(database, `lobbies/${code}/players/${player.id}`), player);
 }
 
@@ -18,4 +36,11 @@ export function listenLobby(code, callback) {
 export async function getLobby(code) {
   const snapshot = await get(ref(database, `lobbies/${code}`));
   return snapshot.val();
+}
+
+export function startLobby(code, draft) {
+  return update(ref(database, `lobbies/${code}`), {
+    status: 'drafting',
+    draft,
+  });
 }
